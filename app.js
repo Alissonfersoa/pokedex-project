09ea1f77ff2cbd0948ff13981fc62c93a03e495f
AlissonPC <batista.als@gmail.com>
2020-11-08 21:18:55 -0300

    Update data
    Refatoração do código

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
-const fetchPokemon = () =>{
+//Refatorando o código para melhor funcionamento
 
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+//referencia as APIs utilizadas
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-    const pokemonPromises = []
+//gera o array para capturar os pokemons
+const generatePokemonsPromises = () => Array(150).fill().map((_, index) => 
+    //função fetch para gerar um json com as informações e atributos de cada pokemon
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
 
-    for (let i =1; i<= 150; i++){
-        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-    }
+//gerador do HTML com as informações de name, id e type para cada pokemon dentro do array
+const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
 
-    Promise.all(pokemonPromises)
-        .then(pokemons => {
-            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+    const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
-                accumulator += `
-                <li class="card ${types[0]}">
-                <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" "/>
-                    <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                    <p class="card-subtitle">${types.join(' | ')}</p>
-                </li>
-                `
-                return accumulator
-            }, '')
+    //gerador de li para leitura do HTML
+    //função src = utiliza uma API para buscar a imagem do pokemon
+    accumulator += `
+        <li class="card ${elementTypes[0]}">
+        <img class="card-image" alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" "/> 
+            <h2 class="card-title">${id}. ${name}</h2>
+            <p class="card-subtitle">${elementTypes.join(' | ')}</p>
+        </li>
+        `
+    return accumulator
+    }, '')
 
-            const ul = document.querySelector('[data-js="pokedex"]')
-
-            ul.innerHTML = lisPokemons
-        })
-
+//gerador do ul dentro do HTML para ter o seletor dos pokemons
+const insertPokemonsIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
 }
 
-fetchPokemon()
+const pokemonPromises = generatePokemonsPromises()
+
+Promise.all(pokemonPromises)
+    .then(generateHTML)
+    .then(insertPokemonsIntoPage)
